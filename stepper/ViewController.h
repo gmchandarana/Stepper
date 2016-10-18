197fcebf233e389a688d97bc57ee32c3787b0d30
Gaurav Chandarana <gmchandarana@gmail.com>
2016-10-18 10:35:47 +0530

    Initial Commit

---
+++ stepper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stepper
+//
+//  Created by Gaurav Chandarana on 05/10/16.
+//  Copyright © 2016 Gaurav Chandarana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ stepper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  stepper
+//
+//  Created by Gaurav Chandarana on 05/10/16.
+//  Copyright © 2016 Gaurav Chandarana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
